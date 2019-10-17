1433c43b58bd243829abb49aeee492a071d6f96c
dani <dani.zaghian@gmail.com>
2019-10-17 11:05:51 -0700

    adds markers and popups

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,6 +15,9 @@
     bottom:0; 
     height:700px;
   }
+  .mapboxgl-popup {
+    max-width: 200px;
+  }
 </style>
 
 <script>
@@ -29,10 +32,32 @@ export default {
     mapboxgl.accessToken = 'pk.eyJ1IjoiZHphZ2hpYW4iLCJhIjoiY2pzbnF0NmV0MGY2czQzbXBpMjcwMzRmNiJ9.Jei4-17Vu7hJSerisjPCEg';
     var map = new mapboxgl.Map({
         container: 'map', // container id
-        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+        style: 'mapbox://styles/dzaghian/cjxaqxm273umk1cqz0saddk4t', // stylesheet location
         center: [-122.431297, 37.773972], // starting position [lng, lat]
         zoom: 12 // starting zoom
     });
+
+    // create the popup
+    var popup = new mapboxgl.Popup({ offset: 25 })
+    .setText("Bob's Donuts, makes big donuts");
+
+    // create another popup
+    var popup2 = new mapboxgl.Popup({ offset: 25 })
+    .setText("Johnny's Donuts, makes great cronuts");
+     
+    // create the marker
+    new mapboxgl.Marker()
+    .setLngLat([-122.421100, 37.791930])
+    .setPopup(popup) // sets a popup on this marker
+    .addTo(map);
+
+    // create another marker
+    new mapboxgl.Marker()
+    .setLngLat([-122.424970, 37.778580])
+    .setPopup(popup2) // sets a popup on this marker
+    .addTo(map);
+
+
   },
   methods: {}
 };
